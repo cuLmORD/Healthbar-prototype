62d103970b3ab0e04a6eaae1c84727453b679b02
Abdullah <mohamanobabdullah@gmail.com>
2023-11-16 20:53:37 +0600

    Healthbar Prototype to test how it works

---
+++ script.js
@@ -0,0 +1,34 @@
+const health = document.querySelector(".health");
+const healthText = document.querySelector(".healthtext");
+let healthDOM = 1;
+let widthDOM = 5;
+const healthDOMtext = "/100";
+
+const addH = () => {
+    widthDOM += 5;
+    health.style.width = widthDOM + "px";
+    healthDOM += 1;
+    healthText.innerHTML = healthDOM + healthDOMtext;
+};
+const remH = () => { 
+    widthDOM -= 5;
+    health.style.width = widthDOM + "px";
+    healthDOM -= 1;
+    healthText.innerHTML = healthDOM + healthDOMtext;
+};
+const addcheck = () => {
+    if (healthDOM <= 99) {
+        addH();
+    } else {
+        healthText.innerHTML = "You're already at max health :D";
+        return;
+    }
+}
+const remcheck = () => {
+    if (healthDOM >= 1) {
+        remH();
+    } else {
+        healthText.innerHTML = "You're already dead bruh";
+        return;
+    }
+}
